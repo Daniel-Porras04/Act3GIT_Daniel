8b9818d038bb85313220109f6f654d48bcf720e5
Daniel Porras <a_daniel.porras.sepulveda@iespablopicasso.es>
2023-11-29 00:16:38 +0100

    2º MODOFICACION AL NUMEROINCREMENTA

--- Act3GitHub/src/NumeroIncrementa.java
+++ Act3GitHub/src/NumeroIncrementa.java
@@ -3,8 +3,28 @@ public class NumeroIncrementa {
   public static void main(String[] args) {
 
     
-    System.out.println("Rama 2");
-    
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+    System.out.println("Rama 2------------------------");
+
   }
 
 }
